32118e8a3e8f92183318e3d23020d1f8505afe58
Gedion Saiyuah <gedionsaiyuah@gmail.com>
2022-01-17 12:18:02 -0800

    fixes

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,14 +1,37 @@
 #include "binary_trees.h"
+#include <stdlib.h>
+
 /**
-*Deletes an entire binary tree
-*@tree is a pointer to the root node of the tree to delete
-*if tree is NULL, do nothing
-*/
-void binary_tree_delete(binary_tree_t *tree);
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree: is a pointer to the root node of the tree to delete
+ * Return: void
+ */
+void binary_tree_delete(binary_tree_t *tree)
 {
-if (tree == NULL)
-return;
-binary_tree_delete(tree->right);
-binary_tree_delete(tree->left);
-free(tree);
+	if (tree)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
+}
+
+/**
+ * delete_helper - can be used in above function to delete nodes
+ * Just another way of implementing a deletion
+ * @tree: pointer to root node
+ * Return: void
+ */
+void delete_helper(binary_tree_t *tree)
+{
+	if (tree->left)
+	{
+		delete_helper(tree->left);
+		free(tree->left);
+	}
+	if (tree->right)
+	{
+		delete_helper(tree->right);
+		free(tree->right);
+	}
 }
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,16 +1,20 @@
 #include "binary_trees.h"
+
 /**
-*checks if a node is a leaf
-*@node is a pointer to the node to check
-*return 1 if node is a leaf, otherwise 0
-*if node is NULL, return 0
-*/
-int binary_tree_is_leaf(const binary_tree_t *node);
+ * binary_tree_is_leaf - checks if a node is a leaf
+ *
+ * @node: a pointer to the node to check
+ *
+ * Return: 1 if node is a leaf, otherwise 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
 if (node == NULL)
 return (0);
-if (node->right == NULL && node->left == NULL)
+
+if (node->left == NULL && node->right == NULL)
+{
 return (1);
-else
+}
 return (0);
 }
